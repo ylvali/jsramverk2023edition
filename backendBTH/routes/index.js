687646b08261499272db404535cbe575ff7be643
Ylvali <ylva@spektatum.com>
2023-11-27 09:59:44 +0530

    Updates on reports : show all and edit

--- backendBTH/routes/index.js
+++ backendBTH/routes/index.js
@@ -8,10 +8,7 @@ router.get('/', function(req, res, next) {
     const data = {
         data: {
             msg: "Hello",
-            pres2: "I am Ylva Sjölin. \
-            I enjoy creating with web programming teqniques like JavaScript & PHP.\
-            And have previously completed 3.5 years in total of information system studies & web technology.\
-            I also enjoy drawing among other things."
+            pres2: "I am Ylva Sjölin. <br> I enjoy creating with web programming teqniques like JavaScript & PHP.<br>And have previously completed 3.5 years in total of information system studies & web technology.<br>I also enjoy drawing among other things."
         }
     };
 
--- backendBTH/routes/reportsReg.js
+++ backendBTH/routes/reportsReg.js
@@ -15,6 +15,38 @@ router.use(bodyParser.urlencoded({ extended: true })); // for parsing applicatio
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/texts.sqlite');
 
+// Route : show all reports
+router.get('/all', function(req, res, next) {
+    var week;
+    var params;
+    var theSql;
+
+    // Call the database with the week nr
+    theSql = "SELECT * FROM 'reports';";
+    params = [];
+
+    db.all(theSql, params, (err, rows)=> {
+            if (err) {
+            console.log('Error: '+err.message);
+            res.status(400).json({"error":err.message});
+            return;
+            }
+
+            if (rows) {
+            console.log (rows);
+            console.log('Success: '+rows);
+            
+            const data = {
+                data: {
+                    msg: 'Success: correct command',
+                    data: rows
+                }
+                };
+                res.json(data);
+            }
+    });
+});
+
 // Route : get report per week
 router.get('/week/:nr', function(req, res, next) {
     var week;
@@ -133,5 +165,84 @@ router.post("/add",
               }
         });
 
+    }
+
+    router.post("/edit", 
+    (req, res, next) => checkToken(req, res, next),
+    (req, res) => editReport(req, res));
+
+    // Checks the token 
+    function checkToken(req, res, next) {
+        const token2 = req.headers['x-access-token'];
+        console.log('Check token');
+        //console.log(req.body);
+        //console.log('TOKEN2:'+token2);
+
+        jwt.verify(token2, process.env.JWT_SECRET, function(err, decoded) {
+            if (err) {
+                // send error response
+                console.log('error token');
+                res.status(201).json({
+                    data: {
+                        msg: "Error token"
+                    }
+                });
+            }
+
+            if (decoded) {
+                // Valid token send on the request
+                console.log('correct token');
+                next();
+               //console.log(addReport);
+            }
+    });
+    }
+
+    function editReport(req, res, next) {
+        console.log('Edit report');
+        console.log(req.body); // Gets the req body
+
+        if (!req.body.data1 || !req.body.title) {
+            res.status(400).json({"error":'no title / data /idNr'});
+            return;
+        }
+
+        var params;
+        var theSql = "UPDATE 'reports' SET data = ? WHERE title = ?;";
+
+        // var theSql = "SELECT * FROM 'reports'";
+        
+        var data = req.body.data1;
+        var title = req.body.title;
+        var idNr = req.body.idNr;
+
+        console.log( 'Data: '+data);
+        console.log( 'Title: '+title);
+        // console.log( 'IdNr: '+idNr);
+
+        params = [data, title];
+
+        // params = [];
+
+        db.all(theSql, params, (err, rows)=> {
+             if (err) {
+                console.log('Error: '+err.message);
+                res.status(400).json({"error":err.message});
+                return;
+              }
+
+              if (rows) {
+                console.log (rows);
+                console.log('Success: '+rows);
+                
+                const data = {
+                  data: {
+                      msg: 'Success: data registered',
+                      data: rows
+                  }
+                 };
+                 res.json(data);
+              }
+        });
     }
 module.exports = router;
